a25defb01de4b54394c99d5f47ebba2c435582f7
habuimanh <hb.buimanhha@gmail.com>
2020-04-25 10:56:36 +0700

    remove eval loop, edit alignment

--- src/main.py
+++ src/main.py
@@ -30,80 +30,79 @@ def main(model_name, Model, Dataset_Loader, breath_type, root_dir, no_outer):
         with open('results/outputs/'+breath_type+'/list_test_eer_GMM_'+model_name, 'rb') as filehandle:
             list_test_eer_GMM = pickle.load(filehandle)
 
-    for eval_time in range(1):
-        # list_train_loss=[]
-        # list_valid_loss=[]
+    # list_train_loss=[]
+    # list_valid_loss=[]
 
-        list_filename=[]
-        for line in open('data/raw/labels/list_'+breath_type+'_filename.txt'):
-            list_filename.append(line.rstrip('\n'))
-        list_train_filename, list_valid_filename, list_test_filename=split_train_valid_test_set(list_filename)
-        list_train_filename, list_valid_filename, list_test_filename, list_outer_valid_filename, list_outer_test_filename, old_new_name_map=get_outer_set(list_train_filename,list_valid_filename,list_test_filename,no_outer=no_outer)
+    list_filename=[]
+    for line in open('data/raw/labels/list_'+breath_type+'_filename.txt'):
+        list_filename.append(line.rstrip('\n'))
+    list_train_filename, list_valid_filename, list_test_filename=split_train_valid_test_set(list_filename)
+    list_train_filename, list_valid_filename, list_test_filename, list_outer_valid_filename, list_outer_test_filename, old_new_name_map=get_outer_set(list_train_filename,list_valid_filename,list_test_filename,no_outer=no_outer)
     
-        model=Model(no_outer=no_outer)
-        if torch.cuda.is_available():
-            model.cuda()
+    model=Model(no_outer=no_outer)
+    if torch.cuda.is_available():
+        model.cuda()
             
-        train_set = Dataset_Loader(root_dir=root_dir,filenamelist=list_train_filename,old_new_name_map=old_new_name_map)
-        train_loader = DataLoader(train_set,batch_size=128, shuffle=True)
+    train_set = Dataset_Loader(root_dir=root_dir,filenamelist=list_train_filename,old_new_name_map=old_new_name_map)
+    train_loader = DataLoader(train_set,batch_size=128, shuffle=True)
 
-        valid_set = Dataset_Loader(root_dir=root_dir,filenamelist=list_valid_filename,old_new_name_map=old_new_name_map)
-        valid_loader = DataLoader(valid_set,batch_size=128, shuffle=True)
+    valid_set = Dataset_Loader(root_dir=root_dir,filenamelist=list_valid_filename,old_new_name_map=old_new_name_map)
+    valid_loader = DataLoader(valid_set,batch_size=128, shuffle=True)
         
-        test_set = Dataset_Loader(root_dir=root_dir,filenamelist=list_test_filename,old_new_name_map=old_new_name_map)
-        test_loader = DataLoader(test_set,batch_size=128, shuffle=True)
+    test_set = Dataset_Loader(root_dir=root_dir,filenamelist=list_test_filename,old_new_name_map=old_new_name_map)
+    test_loader = DataLoader(test_set,batch_size=128, shuffle=True)
 
-        optimizer=torch.optim.Adam(model.parameters(), lr=0.001)
-        early_stopping = EarlyStopping(checkpoint_name = 'results/models/'+breath_type+'/'+model_name+'_'+'checkpoint.pt', 
-            lr_scheduler = StepLR(optimizer, step_size=1, gamma=0.5), patiences=[20, 15, 10, 5])
+    optimizer=torch.optim.Adam(model.parameters(), lr=0.001)
+    early_stopping = EarlyStopping(checkpoint_name = 'results/models/'+breath_type+'/'+model_name+'_'+'checkpoint.pt', 
+        lr_scheduler = StepLR(optimizer, step_size=1, gamma=0.5), patiences=[20, 15, 10, 5])
         
-        coverage = False
-        for epoch in range(1001):
-            train(train_loader=train_loader,model=model,loss_fn=nn.CrossEntropyLoss(),optimizer=optimizer,epoch=epoch)
+    coverage = False
+    for epoch in range(1001):
+        train(train_loader=train_loader,model=model,loss_fn=nn.CrossEntropyLoss(),optimizer=optimizer,epoch=epoch)
 
-            train_losses, train_correct = test(test_loader=train_loader,model=model,loss_fn=nn.CrossEntropyLoss())
-            valid_losses, valid_correct = test(test_loader=valid_loader,model=model,loss_fn=nn.CrossEntropyLoss())
-            # print('Valid set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
-            #     np.mean(valid_losses), valid_correct, len(valid_loader.dataset),
-            #     100. * valid_correct / len(valid_loader.dataset))) 
-            
-            # list_train_loss.append(np.mean(train_losses))
-            # list_valid_loss.append(np.mean(valid_losses))
-
-            if coverage == False and (train_correct / len(train_loader.dataset)) > 0.9:
-                coverage = True
-            
-            if coverage == True:
-                early_stopping(np.mean(valid_losses), model)
-                if early_stopping.early_stop:
-                    break
-
-        model.load_state_dict(torch.load('results/models/'+breath_type+'/'+model_name+'_'+'checkpoint.pt'))
         train_losses, train_correct = test(test_loader=train_loader,model=model,loss_fn=nn.CrossEntropyLoss())
-        print('Train set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
-            np.mean(train_losses), train_correct, len(train_loader.dataset),
-            100. * train_correct / len(train_loader.dataset)))
+        valid_losses, valid_correct = test(test_loader=valid_loader,model=model,loss_fn=nn.CrossEntropyLoss())
+        # print('Valid set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        #     np.mean(valid_losses), valid_correct, len(valid_loader.dataset),
+        #     100. * valid_correct / len(valid_loader.dataset))) 
+            
+        # list_train_loss.append(np.mean(train_losses))
+        # list_valid_loss.append(np.mean(valid_losses))
 
-        test_losses, test_correct = test(test_loader=test_loader,model=model,loss_fn=nn.CrossEntropyLoss())
-        print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
-            np.mean(test_losses), test_correct, len(test_loader.dataset),
-            100. * test_correct / len(test_loader.dataset)))   
-        list_test_acc.append(100. * test_correct / len(test_loader.dataset))
+        if coverage == False and (train_correct / len(train_loader.dataset)) > 0.9:
+            coverage = True
+            
+        if coverage == True:
+            early_stopping(np.mean(valid_losses), model)
+            if early_stopping.early_stop:
+                break
+
+    model.load_state_dict(torch.load('results/models/'+breath_type+'/'+model_name+'_'+'checkpoint.pt'))
+    train_losses, train_correct = test(test_loader=train_loader,model=model,loss_fn=nn.CrossEntropyLoss())
+    print('Train set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        np.mean(train_losses), train_correct, len(train_loader.dataset),
+        100. * train_correct / len(train_loader.dataset)))
+
+    test_losses, test_correct = test(test_loader=test_loader,model=model,loss_fn=nn.CrossEntropyLoss())
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        np.mean(test_losses), test_correct, len(test_loader.dataset),
+        100. * test_correct / len(test_loader.dataset)))   
+    list_test_acc.append(100. * test_correct / len(test_loader.dataset))
         
-        list_test_filename += list_outer_test_filename
-        test_set = Dataset_Loader(root_dir=root_dir,filenamelist=list_test_filename,old_new_name_map=old_new_name_map)
-        test_loader = DataLoader(test_set,batch_size=128, shuffle=True)
-        eer_KNN,eer_GMM=verification_evaluate(train_loader,test_loader,model,no_outer)
-        list_test_eer_KNN.append(eer_KNN)
-        list_test_eer_GMM.append(eer_GMM)
+    list_test_filename += list_outer_test_filename
+    test_set = Dataset_Loader(root_dir=root_dir,filenamelist=list_test_filename,old_new_name_map=old_new_name_map)
+    test_loader = DataLoader(test_set,batch_size=128, shuffle=True)
+    eer_KNN,eer_GMM=verification_evaluate(train_loader,test_loader,model,no_outer)
+    list_test_eer_KNN.append(eer_KNN)
+    list_test_eer_GMM.append(eer_GMM)
         
-        # plt.savefig('results/outputs/multi_modality_grad_TCN.png')
-        # plt.clf()
-        # plt.plot(list_train_loss,color='blue', label = 'train loss')
-        # plt.plot(list_valid_loss,color='green', label = 'valid loss')
-        # plt.savefig('results/outputs/acce_gyro_loss.png')
-        # exit()
-        # torch.save(model, "checkpoints/model_epoch_"+str(eval_time))
+    # plt.savefig('results/outputs/multi_modality_grad_TCN.png')
+    # plt.clf()
+    # plt.plot(list_train_loss,color='blue', label = 'train loss')
+    # plt.plot(list_valid_loss,color='green', label = 'valid loss')
+    # plt.savefig('results/outputs/acce_gyro_loss.png')
+    # exit()
+    # torch.save(model, "checkpoints/model_epoch_"+str(eval_time))
   
     with open('results/outputs/'+breath_type+'/list_test_acc_'+model_name, 'wb') as filehandle:
         pickle.dump(list_test_acc, filehandle)
